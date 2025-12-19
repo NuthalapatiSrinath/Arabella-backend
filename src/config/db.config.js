a87d292525028ea1b627cb2dfb3b7fc3bac0a434
Srinath Nuthalapati <srynath13@gmail.com>
2025-12-19 16:18:54 +0530

    done

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,5 +1,5 @@
 export const dbConfig = {
   url:
     process.env.DATABASE_URL ||
-    "mongodb+srv://Srinath:Srinath@cluster0.4cgz4hy.mongodb.net/",
+    "mongodb+srv://Srinath:Srynath@cluster0.hiskian.mongodb.net/",
 };
